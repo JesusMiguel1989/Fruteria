7aa899350f1d9a6791157b1406e25faed59769a5
SergioMc <carrascosa_96@hotmail.es>
2022-06-15 15:11:02 +0200

    cambios 15-06-22

---
+++ src/main/java/com/practicasjunio2022/fruteria/model/User.java
@@ -0,0 +1,34 @@
+package com.practicasjunio2022.fruteria.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "USER")
+public class User implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4806920792104736164L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
--- src/main/java/com/practicasjunio2022/fruteria/rest/UsersRestController.java
+++ src/main/java/com/practicasjunio2022/fruteria/rest/UsersRestController.java
@@ -1,15 +1,34 @@
 package com.practicasjunio2022.fruteria.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/users")
+import com.practicasjunio2022.fruteria.repo.UserRepository;
+
+@RestController 
+@RequestMapping("/users")
 public class UsersRestController {
-	@RequestMapping(path = "%all", method = RequestMethod.GET)
-	public void getUsers() {
+	
+	@Autowired
+	private UserRepository userRepository;
+	//@RequestMapping(path = "%all", method = RequestMethod.POST)
+	//@RequestMapping("/users/all")
+	@GetMapping("/all")
+	public String getUsers() {
+		this.userRepository.
+		//System.out.println("getusers funciona");
+		return "getusers funciona";
+	}
+	
+	@GetMapping("/get/{id}")
+	public String getUserById(@PathVariable(name = "id") int id) {
 		
-		System.out.println("getusers funciona");
-		
+		//System.out.println("getusers funciona");
+		return "getUser funciona"+id;
 	}
 }
