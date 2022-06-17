43fd981e10cdf09c07f1281f99a5c38a8e5cb775
SergioMc <carrascosa_96@hotmail.es>
2022-06-17 12:39:24 +0200

    Proyecto jonatan

--- src/main/java/com/practicasjunio2022/fruteria/model/User.java
+++ src/main/java/com/practicasjunio2022/fruteria/model/User.java
@@ -8,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity(name = "USER")
+@Entity(name = "usuarios")
 public class User implements Serializable{
 
 	/**
